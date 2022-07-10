c79a991f2bde70d7c7e0b66897db473f1d7036e2
rednatu <rediet.f2016@gmail.com>
2022-07-09 17:02:36 -0700

    help

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *
- * _isupper - evalue if letter is uppercase .
- *
- * @c: print int
- *
- * Return: Always 0.
- *
- */
+*
+* _isupper - evalue if letter is uppercase .
+*
+* @c: print int
+*
+* Return: Always 0.
+*
+*/
 
 
 
